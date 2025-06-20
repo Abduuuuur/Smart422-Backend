3abe8bc08e34b6775fc1a44e133bd37a74835c77
Abdudev <abderrazzaqjalil@gmail.com>
2025-06-20 11:02:43 +0100

    V1.3.1

--- config/db.js
+++ config/db.js
@@ -1,11 +1,20 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
+// Check if you're in production (Railway or Vercel)
+const isProduction = process.env.NODE_ENV === 'production';
+
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
-  protocol: 'postgres',
-  
   logging: false,
+  dialectOptions: isProduction
+    ? {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false
+        }
+      }
+    : {}
 });
 
 module.exports = sequelize;
