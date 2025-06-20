e067c7133bdbf8340d66f1235aa409207b6d466e
Abdudev <abderrazzaqjalil@gmail.com>
2025-06-20 12:44:09 +0100

    V1.3.3.3

--- app.js
+++ app.js
@@ -14,7 +14,10 @@ const weatherRoutes = require('./routes/weatherRoutes');
 
  // <-- create app here BEFORE using it
 
-app.use(cors());
+ app.use(cors({
+  origin: 'https://ysmart422-project-k676.vercel.app',
+  credentials: true,
+}));
 
 app.use(express.json());
 app.use('/api/weather', weatherRoutes);
@@ -22,10 +25,10 @@ app.use('/api/weather', weatherRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api/user', userRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 sequelize.sync().then(() => {
-  app.listen(PORT, () => {
+  app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server running on port ${PORT}`);
   });
 });
