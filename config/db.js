768f2280f9c3717278008baf93557f9957a0d6e8
Abdudev <abderrazzaqjalil@gmail.com>
2025-06-20 10:47:52 +0100

    V1.3

--- config/db.js
+++ config/db.js
@@ -4,12 +4,7 @@ require('dotenv').config();
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
   protocol: 'postgres',
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false,  // important for cloud providers like Supabase
-    }
-  },
+  
   logging: false,
 });
 
